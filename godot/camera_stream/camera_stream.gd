c50bea641b4b7866475194792355b8b607fb57cc
trflorian <florian.trautweiler@hispeed.ch>
2024-06-09 23:19:15 +0200

    simplify camera stream logic without threads

--- godot/camera_stream/camera_stream.gd
+++ godot/camera_stream/camera_stream.gd
@@ -2,36 +2,19 @@ extends TextureRect
 
 var server: UDPServer
 
-var is_running: bool = true
-var accept_connection_thread: Thread
-
-var frame_data: PackedByteArray
-var frame_mutex: Mutex = Mutex.new()
-
 func _ready() -> void:
 	server = UDPServer.new()
 	server.listen(4242)
-	
-	accept_connection_thread = Thread.new()
-	accept_connection_thread.start(_accept_connection_loop)
+
+func _decode_image(frame_data: PackedByteArray) -> Image:
+	var image = Image.new()
+	image.load_jpg_from_buffer(frame_data)
+	return image
 
 func _process(delta: float) -> void:
-	frame_mutex.lock()
-	if len(frame_data) > 0:
-		var image = Image.new()
-		image.load_jpg_from_buffer(frame_data)
+	server.poll()
+	if server.is_connection_available():
+		var peer = server.take_connection()
+		var frame_data = peer.get_packet()
+		var image = _decode_image(frame_data)
 		texture = ImageTexture.create_from_image(image)
-	frame_mutex.unlock()
-
-func _accept_connection_loop():
-	while is_running:
-		server.poll()
-		if server.is_connection_available():
-			var peer: PacketPeerUDP = server.take_connection()
-			frame_mutex.lock()
-			frame_data = peer.get_packet()
-			frame_mutex.unlock()
-
-func _exit_tree():
-	is_running = false
-	accept_connection_thread.wait_to_finish()
